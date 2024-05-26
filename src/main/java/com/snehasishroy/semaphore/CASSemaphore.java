d2b539b2c8df350eb3990a790c1f29459ca2470c
Snehasish Roy <snehasishroy39@gmail.com>
2024-05-26 22:47:56 +0530

    Update log messages

--- src/main/java/com/snehasishroy/semaphore/CASSemaphore.java
+++ src/main/java/com/snehasishroy/semaphore/CASSemaphore.java
@@ -5,8 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
-public class CASSemaphore implements CustomSemaphore {
-    final AtomicInteger tokens;
+public final class CASSemaphore implements CustomSemaphore {
+    private final AtomicInteger tokens;
 
     public CASSemaphore(int count) {
         tokens = new AtomicInteger(count);
@@ -17,8 +17,14 @@ public class CASSemaphore implements CustomSemaphore {
         while (true) {
             int available = tokens.get();
             int remaining = available - 1;
-            if (remaining >= 0 && tokens.compareAndSet(available, remaining)) {
-                return;
+            if (remaining >= 0) {
+                log.info("Tokens are available, trying to perform a CAS");
+                if (tokens.compareAndSet(available, remaining)) {
+                    log.info("Tokens acquired using CAS");
+                    return;
+                }
+            } else {
+                log.info("No tokens available, Retrying");
             }
         }
     }
--- src/main/java/com/snehasishroy/semaphore/WaitNotifySemaphore.java
+++ src/main/java/com/snehasishroy/semaphore/WaitNotifySemaphore.java
@@ -3,10 +3,23 @@ package com.snehasishroy.semaphore;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class WaitNotifySemaphore implements CustomSemaphore {
+public final class WaitNotifySemaphore implements CustomSemaphore {
 
     private final Object lock;
-    volatile int tokens;
+    /**
+     * No point in making this volatile as we already have a lock that causes happens before relationship.
+     * Synchronized methods and blocks provide mutual exclusion guarantees and visibility guarantees i.e. that the changes made by one
+     * thread to the shared data are visible to other threads to maintain data consistency.
+     * Volatile is useful in scenarios where there is no mutual exclusion, but you still want the visibility aspect of the data change.
+     * Volatile keyword introduces the happens before relationship - anything that has happened before writing to a volatile field, will be
+     * visible to the another thread reading the same volatile field. So if there is only one volatile field and thread 1 makes changes to 3 other
+     * non-volatile fields, post which a second thread reads the same volatile field - changes to other non-volatile fields will also be visible.
+     *
+     * Practically volatile should be used when setting a flag or a value - as a simple signalling mechanism - so that value to is visible to other threads.
+     * If you are doing a read, modify, write, then mutual exclusion should be used to avoid lost updates.
+     * <a href="https://www.baeldung.com/java-volatile">Reference</a>
+     */
+    private int tokens; //
 
     public WaitNotifySemaphore(int count) {
         tokens = count;
@@ -19,7 +32,8 @@ public class WaitNotifySemaphore implements CustomSemaphore {
             while (tokens <= 0) {
                 // while loop is critical to avoid any spurious wakeups
                 // in my initial implementation, I missed adding while loop which caused the tests to fail because the acquire
-                // method used to simply return without doing anything and the test used to release the lock causing more tokens than allowed
+                // method used to simply return without doing anything (in case of spurious wakeups) and the test used to
+                // release the lock causing more tokens than allowed.
                 log.info("No token available, waiting for someone to release");
                 lock.wait();
             }
@@ -33,7 +47,7 @@ public class WaitNotifySemaphore implements CustomSemaphore {
         synchronized (lock) {
             tokens++;
             log.info("Released token. Notifying the threads blocked");
-            lock.notifyAll();
+            lock.notify(); // No point in calling notifyAll() as only one
         }
     }
 }
